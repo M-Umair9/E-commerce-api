438f3c7fdb0c2b82aa43f0ff0f6e71e1938c58ed
Umair <ch.umair111353@gmail.com>
2024-09-29 19:52:57 +0500

    Add new authMiddleware.js

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/UserModel.js';
+
+export const protect = async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      // Extract the token from the Authorization header
+      token = req.headers.authorization.split(' ')[1];
+
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET || 'your_jwt_secret');
+
+      // Fetch user data from the database and exclude the password field
+      req.user = await User.findById(decoded.id).select('-password');
+
+      // Proceed to the next middleware or route handler
+      next();
+    } catch (error) {
+      // If token verification fails
+      res.status(401).json({ message: 'Not authorized, token failed' });
+    }
+  }
+
+  if (!token) {
+    // If no token is provided
+    res.status(401).json({ message: 'Not authorized, no token' });
+  }
+};
