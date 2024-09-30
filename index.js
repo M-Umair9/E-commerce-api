bc1c9ec108f702a8c9430b2b5173dc4250ddddbe
Umair <ch.umair111353@gmail.com>
2024-09-30 21:02:25 +0500

    updated index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import authRoutes from './routes/authRoutes.js'; 
 import dotenv from 'dotenv';
+import router from './routes/authRoutes.js';
 dotenv.config(); 
 
 const app =express();
@@ -10,7 +11,7 @@ app.use(express.json());
  
 
    //database connection  
-main().catch(err=>console.log(err));                //call main and catch error
+main().catch(err=>console.log(err)) ;                //call main and catch error
  async function main() {                                     //function to connect to mongoose
    const mongoUri= process.env.MONGO_URI                        
     await mongoose.connect(mongoUri); 
@@ -18,23 +19,10 @@ main().catch(err=>console.log(err));                //call main and catch error
  }
    
  app.use('/api/auth',authRoutes)
-
+app.use(router);
 
 
 app.listen(8080,()=>{
     console.log('server started');
 })
 
-// Route to handle POST request
-{/**server.post('/add-user', async (req, res) => {
-    const { name, email, password } = req.body;
-  
-    try {
-      const newUser = new User({ name, email, password });   // Create a new user from the request body
-      const savedUser = await newUser.save();               // Save the user to the database
-      res.status(201).json(savedUser);
-    } catch (error) {
-      res.status(500).json({ error: 'Error adding user' });
-    }
-  }); */}
-  
