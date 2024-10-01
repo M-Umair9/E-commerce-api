8ac34dd8b61ab411fff73b6915dd2d40177924f7
Umair <ch.umair111353@gmail.com>
2024-10-01 13:04:07 +0500

    updated productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -76,12 +76,12 @@ export const deleteProduct = async (req, res) => {
   try {
     const product = await Product.findById(req.params.id);
     if (product) {
-      await product.remove();
+      await product.deleteOne({ _id: req.params.id });
       res.json({ message: 'Product removed' });
     } else {
       res.status(404).json({ message: 'Product not found' });
     }
   } catch (error) {
-    res.status(500).json({ message: 'Error deleting product', error });
+    res.status(500).json({ message: 'Error deleting product', error:error.message });
   }
 };
