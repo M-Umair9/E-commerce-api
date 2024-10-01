85d105b310080b6117cc807d266e8410394a5e08
Umair <ch.umair111353@gmail.com>
2024-10-01 13:07:06 +0500

    Add new productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,15 +6,15 @@ import {
   updateProduct,
   deleteProduct,
 } from '../controllers/productController.js';
-import { protect, admin } from '../middleware/authMiddleware.js';
+
 
 const productrouter = express.Router();
 
 // CRUD routes for products
-productrouter.post('/products', protect, admin, createProduct);  // Create product (Admin only)
+productrouter.post('/products',  createProduct);  // Create product (Admin only) 
 productrouter.get('/getproducts', getProducts);                     // Get all products
 productrouter.get('/getproducts/:id', getProductById);               // Get product by ID
-productrouter.put('/updateproducts/:id', protect, admin, updateProduct); // Update product (Admin only)
-productrouter.delete('deleteproducts/:id', protect, admin, deleteProduct); // Delete product (Admin only)
+productrouter.put('/updateproducts/:id',updateProduct ); // Update product (Admin only) , admin,  protect
+productrouter.delete('/deleteproducts/:id', deleteProduct); // Delete product (Admin only) , admin,  protect
 
 export default productrouter;
