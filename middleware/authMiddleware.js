7358297d0bdeb66c255ef05271177d5b4ede098a
Umair <ch.umair111353@gmail.com>
2024-10-01 11:35:28 +0500

    Updated middleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -28,3 +28,12 @@ export const protect = async (req, res, next) => {
     res.status(401).json({ message: 'Not authorized, no token' });
   }
 };
+  
+//auth for Admin
+export const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(403).json({ message: 'Not authorized as admin' });
+  }
+};
