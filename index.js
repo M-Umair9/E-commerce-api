c3b03ada23603f6570271ca0dc74af28a26f4bce
Umair <ch.umair111353@gmail.com>
2024-10-02 19:19:02 +0500

    Updated index.js

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ import cartRouters from './routes/cartRoutes.js';
 import cartrouter from './routes/cartRoutes.js';
 import orderrouter from './routes/orderRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
+import paymentrouter from './routes/paymentRoutes.js';
+import paymentRouters from './routes/paymentRoutes.js';
 dotenv.config(); 
 
 const app =express();
@@ -28,10 +30,12 @@ app.use(router);
 app.use(productrouter);
 app.use(cartrouter);
 app.use(orderrouter);
+app.use(paymentrouter);
 app.use('/api/auth',authRoutes)                  //Add Auth routes
 app.use('/api/products', productRoutes);         // Add product routes
 app.use('/api/cart', cartRouters);
 app.use('/api/order',orderRouter);
+app.use('/api/payment',paymentRouters);
 
 app.listen(8080,()=>{
     console.log('server started');
